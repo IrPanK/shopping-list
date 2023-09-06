5b709f85b472dcbd1164a1fb340241baa2fd29c8
IrPanTech <irfannkamill@gmail.com>
2023-09-06 15:28:49 +0700

    tutorial 1

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def show_main(request):
+    context = {"name": "Irfan", "class": "PBP E"}
+
+    return render(request, "main.html", context)
