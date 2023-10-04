6cd18f6a6b051e25cb181643043931d3685184c5
IrPanTech <irfannkamill@gmail.com>
2023-10-04 15:32:43 +0700

    filter product by user

--- main/views.py
+++ main/views.py
@@ -138,7 +138,7 @@ def logout_user(request):
 
 
 def get_product_json(request):
-    product_item = Product.objects.all()
+    product_item = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", product_item))
 
 
